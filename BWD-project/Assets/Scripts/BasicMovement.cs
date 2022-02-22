86653ac661a983ca6813f1fc38454059002d996d
Wessel van Zeggeren <wessel.van.zeggeren@gmail.com>
2022-02-22 14:57:45 +0100

    Started with basic setup work
    Added the movable pill, a black skybox and a fixed camera. This already works better then expected

---
+++ BWD-project/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+    public float speed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        this.updateMovement();
+    }
+
+    private void updateMovement()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(x, 0.0f, z);
+
+        transform.position += (direction * this.speed);
+    }
+}
